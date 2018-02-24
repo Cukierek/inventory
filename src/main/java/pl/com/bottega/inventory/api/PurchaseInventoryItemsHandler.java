165ae41b8d5ff3783dff0d5ff971d0e9d38884c1
Cukierek <cukierek@programmer.net>
2018-02-24 22:00:28 +0100

    more refactoring, optimizing purchase query

--- src/main/java/pl/com/bottega/inventory/api/PurchaseInventoryItemsHandler.java
+++ src/main/java/pl/com/bottega/inventory/api/PurchaseInventoryItemsHandler.java
@@ -8,6 +8,7 @@ import pl.com.bottega.inventory.domain.commands.PurchaseInventoryItemsCommand;
 import pl.com.bottega.inventory.domain.commands.Validatable;
 
 import javax.transaction.Transactional;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
 
@@ -24,15 +25,16 @@ public class PurchaseInventoryItemsHandler implements Handler<PurchaseInventoryI
 	@Transactional
 	public PurchaseDto handle(PurchaseInventoryItemsCommand command) {
 		Validatable.ValidationErrors validationErrors = new Validatable.ValidationErrors();
+		Map<String, InventoryItem> localInventoryMap = new HashMap<>();
 		PurchaseDto purchaseDto;
 		PurchaseSuccessDto successDto = new PurchaseSuccessDto();
 		PurchaseFailedDto failedDto = new PurchaseFailedDto();
 		command.getProducts().entrySet().stream().forEach(pair -> {
-			processPair(validationErrors, successDto, failedDto, pair);
+			processPair(validationErrors, successDto, failedDto, pair, localInventoryMap);
 		});
 		if (failedDto.getMissingProducts().isEmpty() && validationErrors.isValid()) {
 			purchaseDto = successDto;
-			persistChanges(command);
+			persistChanges(command, localInventoryMap);
 		} else {
 			purchaseDto = failedDto;
 		}
@@ -40,22 +42,30 @@ public class PurchaseInventoryItemsHandler implements Handler<PurchaseInventoryI
 		return purchaseDto;
 	}
 
-	private void processPair(Validatable.ValidationErrors validationErrors, PurchaseSuccessDto successDto, PurchaseFailedDto failedDto, Map.Entry<String, Integer> pair) {
+	private void processPair(Validatable.ValidationErrors validationErrors, PurchaseSuccessDto successDto,
+	                         PurchaseFailedDto failedDto, Map.Entry<String, Integer> pair,
+	                         Map<String, InventoryItem> localInventoryMap) {
 		String requestedSkuCode = pair.getKey();
 		Integer requestedAmount = pair.getValue();
 		Optional<InventoryItem> inventoryItem = inventoryItemRepository.findBySkuCode(requestedSkuCode);
-		validateForPresence(validationErrors, successDto, failedDto, requestedSkuCode, requestedAmount, inventoryItem);
+		validateForPresence(validationErrors, successDto, failedDto, requestedSkuCode,
+				requestedAmount, inventoryItem, localInventoryMap);
 	}
 
-	private void validateForPresence(Validatable.ValidationErrors validationErrors, PurchaseSuccessDto successDto, PurchaseFailedDto failedDto, String requestedSkuCode, Integer requestedAmount, Optional<InventoryItem> inventoryItem) {
+	private void validateForPresence(Validatable.ValidationErrors validationErrors, PurchaseSuccessDto successDto,
+	                                 PurchaseFailedDto failedDto, String requestedSkuCode, Integer requestedAmount,
+	                                 Optional<InventoryItem> inventoryItem,
+	                                 Map<String, InventoryItem> localInventoryMap) {
 		if (isPresentInRepository(inventoryItem)) {
+			localInventoryMap.put(requestedSkuCode, inventoryItem.get());
 			validateStock(successDto, failedDto, requestedSkuCode, requestedAmount, inventoryItem);
 		} else {
 			validationErrors.add(requestedSkuCode, "no such sku");
 		}
 	}
 
-	private void validateStock(PurchaseSuccessDto successDto, PurchaseFailedDto failedDto, String requestedSkuCode, Integer requestedAmount, Optional<InventoryItem> inventoryItem) {
+	private void validateStock(PurchaseSuccessDto successDto, PurchaseFailedDto failedDto,
+	                           String requestedSkuCode, Integer requestedAmount, Optional<InventoryItem> inventoryItem) {
 		InventoryItem item = inventoryItem.get();
 		if (item.getAmount() < requestedAmount) {
 			failedDto.getMissingProducts().put(requestedSkuCode, requestedAmount);
@@ -64,16 +74,13 @@ public class PurchaseInventoryItemsHandler implements Handler<PurchaseInventoryI
 		}
 	}
 
-	private void persistChanges(PurchaseInventoryItemsCommand command) {
+	private void persistChanges(PurchaseInventoryItemsCommand command, Map<String, InventoryItem> localInventoryMap) {
 		command.getProducts().entrySet().stream().forEach(pair -> {
 			String requestedSkuCode = pair.getKey();
 			Integer requestedAmount = pair.getValue();
-			Optional<InventoryItem> inventoryItem = inventoryItemRepository.findBySkuCode(requestedSkuCode);
-			if (isPresentInRepository(inventoryItem)) {
-				InventoryItem item = inventoryItem.get();
-				item.decreaseStock(requestedAmount);
-				inventoryItemRepository.save(item);
-			}
+			InventoryItem item = localInventoryMap.get(requestedSkuCode);
+			item.decreaseStock(requestedAmount);
+			inventoryItemRepository.save(item);
 		});
 	}
 
